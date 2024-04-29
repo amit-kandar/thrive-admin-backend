86eebcb1a2a91c19b7a2c81ff7953c202616c22e
Amit Kandar <kandaramit2001@gmail.com>
2024-04-30 02:07:53 +0530

    added about us route

--- src/routes/AboutUs.routes.js
+++ src/routes/AboutUs.routes.js
@@ -1,8 +1,16 @@
 const express = require('express');
-const aboutUs = require('../controllers/AboutUs.controller');
+const { getData, addData, updateData } = require('../controllers/AboutUs.controller');
+const upload = require('../middlewares/multer.middleware');
 
 const router = express.Router();
-
-router.put('/update', aboutUs);
+router.post('/', upload.fields([
+    { name: "image", maxCount: 1 },
+    { name: "cover_image", maxCount: 1 }
+]), addData)
+router.get('/', getData)
+router.put('/', upload.fields([
+    { name: "image", maxCount: 1 },
+    { name: "cover_image", maxCount: 1 }
+]), updateData);
 
 module.exports = router;
