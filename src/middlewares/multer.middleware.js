afa6207c57f594aec445e039abc7c5d85842bf3c
Amit Kandar <kandaramit2001@gmail.com>
2024-04-30 02:06:45 +0530

    added multer middleware

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,27 @@
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './public/temp');
+    },
+    filename: function (req, file, cb) {
+        // Get the original filename without extension
+        const originalNameWithoutExt = path.parse(file.originalname).name;
+
+        // Generate a random 6-digit number
+        const randomSixDigits = Math.floor(100000 + Math.random() * 900000);
+
+        // Get the file extension
+        const ext = path.extname(file.originalname);
+
+        // Construct the filename with original name and random number
+        const fileName = `${originalNameWithoutExt}_${randomSixDigits}${ext}`;
+
+        cb(null, fileName);
+    }
+});
+
+const upload = multer({ storage: storage });
+
+module.exports = upload;
