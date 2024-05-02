6f72f14504d856a0bd3f73590dbc93504bfdc8d2
Amit Kandar <kandaramit2001@gmail.com>
2024-05-02 22:46:21 +0530

    Improved the env configuration and add database function

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-const dotenv = require("dotenv");
+require('dotenv').config();
 const app = require("./app.js");
 const cloudinary = require("cloudinary").v2;
 const logger = require("./config/logger.js");
-
-dotenv.config({
-    path: "./.env"
-});
+const db = require('./db')
 
 const PORT = process.env.PORT || 8000;
 
@@ -16,10 +13,22 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET || ''
 });
 
-app.on("error", err => {
-    logger.error("Error: ", err);
-});
+// Connect to the database
+db.connectToDatabase()
+    .then(() => {
+        // Sync models with database
+        return db.synchronizeDatabase();
+    })
+    .then(() => {
+        app.on("error", err => {
+            logger.error("Error: ", err);
+        });
+        app.listen(PORT, () => {
+            logger.info(`server is running at port ${PORT}`);
+        });
+    })
+    .catch(error => {
+        logger.error('Error starting the server:', error);
+    });
 
-app.listen(PORT, () => {
-    logger.info(`server is running at port ${PORT}`);
-});
+
