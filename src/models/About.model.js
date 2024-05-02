74cda9b31d9a4dedcb8cfd4ff3b09877bc921074
Amit Kandar <kandaramit2001@gmail.com>
2024-05-02 23:04:03 +0530

    Update the model

---
+++ src/models/About.model.js
@@ -0,0 +1,45 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    const About = sequelize.define('About', {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        sub_title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        points: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        has_cta: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false
+        },
+        cta_text: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        cover_image: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        image: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        image_public_id: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        cover_image_public_id: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    }, {
+        timestamps: true
+    });
+    return About;
+};
