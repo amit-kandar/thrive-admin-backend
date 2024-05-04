468cd76fd9f9bff23040a3ce16bef81a76a2d5d9
Amit Kandar <kandaramit2001@gmail.com>
2024-05-05 00:51:31 +0530

    Added course controller

--- src/routes/Course.routes.js
+++ src/routes/Course.routes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import addCourse from '../controllers/Course.controller.js';
+import { addCourse, getCourse, updateCourse } from '../controllers/Course.controller.js';
 import upload from '../middlewares/multer.middleware.js';
 
 const router = express.Router();
 
 router.post('/', upload.single('image'), addCourse);
+router.put('/', upload.single('image'), updateCourse);
+router.get('/', getCourse);
 
 export default router;
