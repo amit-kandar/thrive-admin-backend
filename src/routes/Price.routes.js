cbbcfe3352a34597c51455681a90a3a054117413
Amit Kandar <kandaramit2001@gmail.com>
2024-05-15 01:55:54 +0530

    Added update price plan and get price plan route

--- src/routes/Price.routes.js
+++ src/routes/Price.routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const { addPricePlan } = require('../controllers/Price.controller.js');
+const { addPricePlan, updatePricePlan, getPricePlanDetails } = require('../controllers/Price.controller.js');
 const router = express.Router();
 
 router.post('/', addPricePlan);
+router.put('/', updatePricePlan);
+router.get('/', getPricePlanDetails);
 
 module.exports = router;
